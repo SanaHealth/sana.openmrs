517169973ba55e4108f3cb2688f6b20243652924
Eric Winkler <winkler.em@gmail.com>
2012-12-07 15:37:18 -0500

    Fixed MIMEType detection

--- omod/src/main/java/org/openmrs/module/sana/queue/web/servlet/EncounterServlet.java
+++ omod/src/main/java/org/openmrs/module/sana/queue/web/servlet/EncounterServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.Integer;
 
+import javax.activation.MimetypesFileTypeMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,23 +28,28 @@ public class EncounterServlet extends HttpServlet {
         Encounter e = Context.getEncounterService().getEncounter(new Integer(encounterID));
         String s = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
         s += "<encounter patientID=\"" + e.getPatientId().toString() + "\" patientFirst=\"" + e.getPatient().getGivenName() + "\" patientLast=\"" + e.getPatient().getFamilyName() + "\">";
-        
+        log.debug("Encounter: %s, " + encounterID);
         for (Obs o : e.getAllObs())
         {
         	if(o.isComplex()){
-        		log.error("type " + o.getValueText());
+
+            	String type = o.getValueComplex();
+        		log.debug("type " + o.getValueComplex() + ", MIME: " + type);
         		//Audio file
-        		if(o.getValueText().equals("SOUND")){
+        		//if(o.getValueText().equals("SOUND")){
+        		if(MIMEUtil.isAudio(o)){	
         			s += "<genericFile type=\"audio\" obsID=\"" + o.getObsId().toString() + "\" summary=\"\"/>";        			
             		log.info("audio obs " + o.toString() + " " + o.isComplex());
         		}
         		//Video file
-        		else if(o.getValueText().equals("VIDEO")){
+        		//else if(o.getValueText().equals("VIDEO")){
+        		else if(MIMEUtil.isVideo(o)){	
         			s += "<genericFile type=\"video\" obsID=\"" + o.getObsId().toString() + "\" summary=\"\"/>";        			
             		log.info("video obs " + o.toString() + " " + o.isComplex());
         		}
         		//Image
-        		else{
+        		//else{
+        		else if(MIMEUtil.isImage(o)){	
         			s += "<genericFile type=\"image\" obsID=\"" + o.getObsId().toString() + "\" summary=\"\"/>";        			
             		log.info("image obs " + o.toString() + " " + o.isComplex());
         		}
@@ -61,4 +67,45 @@ public class EncounterServlet extends HttpServlet {
   
     }
     
+
+    
+    public static class MIMEUtil{
+    	final static String[] AUDIO = {"wav", "mp3" };
+    	final static String[] IMAGE = {"jpg", "bmp"};
+    	final static String[] VIDEO = {"mpg", "flv"};
+    	
+        public static String getMimeType(Obs o){
+        	MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+        	String uri = o.getValueComplex().split("|")[1].trim();
+        	String mimeType = mimeTypesMap.getContentType(uri);
+    		return mimeType;
+        }
+        
+        public static boolean isAudio(Obs o){
+        	String type = o.getValueComplex();
+        	if(type.indexOf("audio") > -1){
+        		return true;
+        	} else 
+        		return false;
+        }
+    	
+        public static boolean isImage(Obs o){
+        	String type = o.getValueComplex();
+        	if(type.indexOf("image") > -1){
+        		return true;
+        	} else 
+        		return false;
+        	
+        }
+        
+        public static boolean isVideo(Obs o){
+        	String type = o.getValueComplex();
+        	if(type.indexOf("video") > -1){
+        		return true;
+        	} else 
+        		return false;
+        	
+        }
+        
+    }
 }
